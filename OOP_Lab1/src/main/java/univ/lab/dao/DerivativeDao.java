09699d21188d98db26a5f4d2e0b526a1f7cc5d8b
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-09-26 02:39:20 +0300

    added derivative dao

--- OOP_Lab1/src/main/java/univ/lab/dao/DerivativeDao.java
+++ OOP_Lab1/src/main/java/univ/lab/dao/DerivativeDao.java
@@ -2,6 +2,5 @@ package univ.lab.dao;
 
 import univ.lab.model.Derivative;
 
-public interface DerivativeDao {
-    Derivative save(Derivative insurance);
+public interface DerivativeDao extends CrudDaoManager<Derivative> {
 }
--- OOP_Lab1/src/main/java/univ/lab/dao/DerivativeDaoImpl.java
+++ OOP_Lab1/src/main/java/univ/lab/dao/DerivativeDaoImpl.java
@@ -2,9 +2,44 @@ package univ.lab.dao;
 
 import univ.lab.model.Derivative;
 
+import java.util.List;
+import java.util.Optional;
+
 public class DerivativeDaoImpl implements DerivativeDao {
+
+    private final CrudDaoManager<Derivative> crudDaoManager;
+
+    public DerivativeDaoImpl(CrudDaoManager<Derivative> crudDaoManager) {
+        this.crudDaoManager = crudDaoManager;
+    }
+
     @Override
     public Derivative save(Derivative insurance) {
-        throw new UnsupportedOperationException("To implement");
+        return crudDaoManager.save(insurance);
+    }
+
+    @Override
+    public List<Derivative> findAll() {
+        return crudDaoManager.findAll();
+    }
+
+    @Override
+    public Optional<Derivative> findById(Long id) {
+        return crudDaoManager.findById(id);
+    }
+
+    @Override
+    public void delete(Long id) {
+        crudDaoManager.delete(id);
+    }
+
+    @Override
+    public void delete(Derivative entity) {
+        crudDaoManager.delete(entity);
+    }
+
+    @Override
+    public Derivative update(Derivative entity) {
+        return crudDaoManager.update(entity);
     }
 }
