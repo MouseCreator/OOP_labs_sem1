a930809d86ae39fa2e9f8b0ed5247e5c5800664e
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-09-25 21:57:38 +0300

    created insurance database

--- OOP_Lab1/src/main/java/univ/lab/util/HibernateUtil.java
+++ OOP_Lab1/src/main/java/univ/lab/util/HibernateUtil.java
@@ -3,15 +3,33 @@ package univ.lab.util;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class HibernateUtil {
 
-    private final static SessionFactory sessionFactory = initSessionFactory();
+    private static SessionFactory sessionFactory = null;
 
     private static SessionFactory initSessionFactory() {
-        return new Configuration().configure().buildSessionFactory();
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+
+        Properties properties = new Properties();
+        try (InputStream input = HibernateUtil.class.getResourceAsStream("/db_config")) {
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to configure Hibernate");
+        }
+
+        configuration.setProperty("hibernate.connection.username", properties.getProperty("db.username"));
+        configuration.setProperty("hibernate.connection.password", properties.getProperty("db.password"));
+        return configuration.buildSessionFactory();
     }
 
     public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null)
+            sessionFactory = initSessionFactory();
         return sessionFactory;
     }
 }
