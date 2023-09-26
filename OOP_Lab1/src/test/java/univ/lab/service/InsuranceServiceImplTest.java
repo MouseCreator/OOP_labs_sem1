fd3e8b1ef5839e9b4f1c49e6d1c774c2b5dc0e63
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-09-26 03:19:26 +0300

    added crud tests for insurance service

--- OOP_Lab1/src/test/java/univ/lab/service/InsuranceServiceImplTest.java
+++ OOP_Lab1/src/test/java/univ/lab/service/InsuranceServiceImplTest.java
@@ -9,6 +9,8 @@ import univ.lab.factory.InsuranceFactoryImpl;
 import univ.lab.model.Insurance;
 import univ.lab.model.LifeInsurance;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InsuranceServiceImplTest {
@@ -31,6 +33,39 @@ class InsuranceServiceImplTest {
         assertNotNull(savedInsurance);
         assertNotNull(savedInsurance.getId());
         assertEquals(lifeInsurance.getRisk(), savedInsurance.getRisk());
+    }
+    @Test
+    void updateInsurance_Ok() {
+        LifeInsurance lifeInsurance =
+                insuranceFactory.getLifeInsurance(10, "Petrov", 20L, "Asteroid");
+        LifeInsurance updated =
+                insuranceFactory.getLifeInsurance(20, "Petrov", 20L, "Asteroid");
+
+        Mockito.when(insuranceService.update(lifeInsurance)).thenReturn(updated);
+
+
+        lifeInsurance.setRisk(lifeInsurance.getRisk()+10);
+
+        Insurance updatedActual = insuranceService.update(lifeInsurance);
+
+        assertEquals(20, updatedActual.getRisk());
+    }
+
+    @Test
+    void findInsurance_Ok() {
+        Long id = 2L;
+
+        LifeInsurance lifeInsurance =
+                insuranceFactory.getLifeInsurance(10, "Petrov", 20L, "Asteroid");
+        setId(lifeInsurance, id);
+
+
+        Mockito.when(insuranceService.find(id)).thenReturn(Optional.of(lifeInsurance));
+
+
+        Optional<Insurance> updatedActual = insuranceService.find(id);
+        assertTrue(updatedActual.isPresent());
+        assertEquals(id, updatedActual.get().getId());
     }
 
     private Insurance setId(LifeInsurance lifeInsurance, Long id) {
