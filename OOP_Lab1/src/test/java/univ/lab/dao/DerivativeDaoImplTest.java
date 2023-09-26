dc845ff072f82b1177c223be4fae3bb1b49e4979
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-09-26 03:58:59 +0300

    added reading tests for derivative dao

--- OOP_Lab1/src/test/java/univ/lab/dao/DerivativeDaoImplTest.java
+++ OOP_Lab1/src/test/java/univ/lab/dao/DerivativeDaoImplTest.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import univ.lab.factory.DerivativeFactory;
 import univ.lab.factory.DerivativeFactoryImpl;
+import univ.lab.factory.InsuranceFactory;
+import univ.lab.factory.InsuranceFactoryImpl;
 import univ.lab.model.Derivative;
 import univ.lab.model.Insurance;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,10 +19,13 @@ class DerivativeDaoImplTest extends InMemoryDatabaseTest {
     private DerivativeDao derivativeDao;
     private DerivativeFactory derivativeFactory;
 
+    private InsuranceFactory insuranceFactory;
+
     @BeforeEach
     void setUp() {
         this.derivativeDao = new DerivativeDaoImpl(new CrudDaoManagerImpl<>(getSessionFactory(), Derivative.class));
         derivativeFactory = new DerivativeFactoryImpl();
+        insuranceFactory = new InsuranceFactoryImpl();
     }
 
     @Test
@@ -37,20 +43,52 @@ class DerivativeDaoImplTest extends InMemoryDatabaseTest {
         assertNotNull(savedDerivative.getInsurances());
     }
 
+    private int injectData() {
+        Derivative derivative1 = derivativeFactory.getDerivative();
+        derivative1.getInsurances().add(
+                insuranceFactory.getLifeInsurance(5, "Alice", 200L, "Ice"));
+        derivative1.getInsurances().add(
+                insuranceFactory.getCarInsurance(4, "Bob", 100L, "AA 00 BB", 200L));
+        derivative1.getInsurances().add(
+                insuranceFactory.getCarInsurance(3, "Carl", 50L, "CC 00 DD", 170L));
+        derivativeDao.save(derivative1);
+
+        Derivative derivative2 = derivativeFactory.getDerivative();
+        derivative2.getInsurances().add(
+                insuranceFactory.getHouseInsurance(5, "Alice", 80L, "Vito street"));
+        derivative2.getInsurances().add(
+                insuranceFactory.getCarInsurance(2, "Bill", 20L, "EE 00 FF", 30L));
+        derivativeDao.save(derivative2);
+        Derivative derivative3 = derivativeFactory.getDerivative();
+        derivativeDao.save(derivative3);
+        return 3;
+    }
     @Test
     void findAll() {
+        int injectedItemsCount = injectData();
+        List<Derivative> derivativeList = derivativeDao.findAll();
+        assertNotNull(derivativeList);
+        assertEquals(injectedItemsCount, derivativeList.size());
+        Long i = 1L;
+        for (Derivative derivative : derivativeList) {
+            assertNotNull(derivative);
+            assertEquals(i, derivative.getId());
+            i++;
+        }
     }
 
     @Test
     void findById() {
+        int injectedItemsCount = injectData();
+        assert injectedItemsCount > 0;
+        Optional<Derivative> derivativeOptional = derivativeDao.findById(1L);
+        assertTrue(derivativeOptional.isPresent());
+        Derivative derivative = derivativeOptional.get();
+        assertEquals(1L, derivative.getId());
     }
 
     @Test
     void delete() {
-    }
-
-    @Test
-    void testDelete() {
     }
 
     @Test
