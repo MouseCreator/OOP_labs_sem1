40cabe807c84aeb24ead84f153ed41967060291a
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-24 08:33:14 +0300

    implemented regular SAX Parser

--- OOP_Lab2/src/main/java/univ/lab/regular/RegularSAXParser.java
+++ OOP_Lab2/src/main/java/univ/lab/regular/RegularSAXParser.java
@@ -1,4 +1,29 @@
 package univ.lab.regular;
 
-public class RegularSAXParser {
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+
+public class RegularSAXParser implements RegularParser {
+    private final SAXParserFactory SAXFactory;
+
+
+    public RegularSAXParser(SAXParserFactory saxFactory) {
+        SAXFactory = saxFactory;
+    }
+
+    @Override
+    public Object parse(String filename) {
+        try {
+            SAXParser saxParser = SAXFactory.newSAXParser();
+            RegularSAXHandler saxPaperHandler = new RegularSAXHandler();
+            saxParser.parse(filename, saxPaperHandler);
+            return saxPaperHandler.getResult();
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
