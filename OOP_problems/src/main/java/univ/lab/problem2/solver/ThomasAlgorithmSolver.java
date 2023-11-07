f72c52974e7f0843dc2c23ecea16933b0ab35be3
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-11-07 06:49:37 +0200

    added thomas algorithm test

--- OOP_problems/src/main/java/univ/lab/problem2/solver/ThomasAlgorithmSolver.java
+++ OOP_problems/src/main/java/univ/lab/problem2/solver/ThomasAlgorithmSolver.java
@@ -3,21 +3,22 @@ package univ.lab.problem2.solver;
 public class ThomasAlgorithmSolver {
 
     private static double calculate_z(double[][] matrix, double[] alpha_vector, int i) {
-        return matrix[i][i] - alpha_vector[i] * matrix[i][i-1];
+        return - (matrix[i][i] + alpha_vector[i] * matrix[i][i-1]);
     }
     public static double[] solve(double[][] matrix, double[] vector) {
         int n = matrix.length;
         double[] alpha_vector = new double[n];
         double[] beta_vector = new double[n];
-        alpha_vector[0] = matrix[0][1] / matrix[0][0];
-        beta_vector[0] = vector[0] / matrix[0][0];
+        alpha_vector[1] = - matrix[0][1] / matrix[0][0];
+        beta_vector[1] = vector[0] / matrix[0][0];
         for (int i = 1; i < n - 1; i++) {
             double z = calculate_z(matrix, alpha_vector, i);
             alpha_vector[i+1] = matrix[i][i+1] / z;
-            beta_vector[i+1] = (vector[i] + matrix[i][i-1]) / z;
+            beta_vector[i+1] = (matrix[i][i-1] * beta_vector[i] - vector[i]) / z;
         }
         double[] result = new double[n];
-        result[n-1] = (vector[n-1] + matrix[n-1][n-2] * beta_vector[n-1]) / calculate_z(matrix, alpha_vector, n-1);
+        int last = n-1;
+        result[last] = (matrix[last][last-1] * beta_vector[last] - vector[last]) / calculate_z(matrix, alpha_vector, last);
 
         for (int i = n - 2; i >= 0; i--) {
             int j = i+1;
---
+++ OOP_problems/src/test/java/univ/lab/problem2/solver/ThomasAlgorithmSolverTest.java
@@ -0,0 +1,35 @@
+package univ.lab.problem2.solver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ThomasAlgorithmSolverTest {
+    private double[][] parseMatrix(String input) {
+        String[] rows = input.trim().split("\n");
+        int numRows = rows.length;
+        int numCols = rows[0].trim().split(" ").length;
+        double[][] matrix = new double[numRows][numCols];
+
+        for (int i = 0; i < numRows; i++) {
+            String[] values = rows[i].trim().split(" ");
+            for (int j = 0; j < numCols; j++) {
+                matrix[i][j] = Double.parseDouble(values[j]);
+            }
+        }
+
+        return matrix;
+    }
+    @Test
+    void solve() {
+        double[][] matrix = parseMatrix("""
+                1 1 0
+                1 3 2
+                0 1 2
+                """);
+        double[] vector = new double[]{1,1,1};
+        double[] solve = ThomasAlgorithmSolver.solve(matrix, vector);
+        double[] solution = new double[]{2,-1,1};
+        assertArrayEquals(solution, solve);
+    }
+}
