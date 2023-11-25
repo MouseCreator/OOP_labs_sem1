e8975d4a54ad040beb113c884a882f4554f201f3
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-11-25 21:37:27 +0200

    added identity distance test

--- Android/DTWTest/app/src/test/java/univ/lab/dtwtest/dtw/DTWTest.java
+++ Android/DTWTest/app/src/test/java/univ/lab/dtwtest/dtw/DTWTest.java
@@ -10,6 +10,14 @@ public class DTWTest extends TestCase {
         DTW<Integer> dtw = new DTW<>((a, b) -> Math.abs(a-b));
         Integer[] a = {1, 7, 4, 8, 2, 9, 6, 5, 2, 0};
         Integer[] b = {1, 2, 8, 5, 5, 1, 9, 4, 6, 5};
-        assertEquals(82, dtw.dtwDistance(a, b));
+        assertEquals(17.0, dtw.dtwDistance(a, b));
+    }
+
+    @Test
+    public void testIdentityDistance() {
+        DTW<Integer> dtw = new DTW<>((a, b) -> Math.abs(a-b));
+        Integer[] a = {1, 7, 4, 8, 2, 9, 6, 5, 2, 0};
+        Integer[] b = {1, 7, 4, 8, 2, 9, 6, 5, 2, 0};
+        assertEquals(0.0, dtw.dtwDistance(a, b));
     }
 }
