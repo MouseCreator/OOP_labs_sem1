8b43358c101b96cff25dd18c7f6c6813f0e128dc
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-11-26 21:50:25 +0200

    added shuriken image with rotation animation

--- Android/NinjaGame1/app/src/main/java/univ/lab/ninjagame1/MainActivity.java
+++ Android/NinjaGame1/app/src/main/java/univ/lab/ninjagame1/MainActivity.java
@@ -1,19 +1,48 @@
 package univ.lab.ninjagame1;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
-import univ.lab.ninjagame1.game.Game;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
-    private static final String DEBUG_TAG = "Velocity";
+
+
+public class MainActivity extends AppCompatActivity {
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new Game(this));
+        setContentView(R.layout.activity_main);
+
+        ImageView imageViewShuriken = findViewById(R.id.shurikenIm);
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setInterpolator(new LinearInterpolator());
+        rotateAnimation.setRepeatCount(Animation.INFINITE);
+        rotateAnimation.setDuration(2000);
+        imageViewShuriken.startAnimation(rotateAnimation);
+        gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                calculateAndDisplaySpeed(velocityY);
+                return true;
+            }
+        });
+
+        imageViewShuriken.setOnTouchListener((v, event) -> {
+            gestureDetector.onTouchEvent(event);
+            return true;
+        });
     }
 
+    private void calculateAndDisplaySpeed(float velocityY) {
+        int speed = Math.abs((int) velocityY);
+        Log.d("Main", "Speed: " + speed);
+    }
 }
