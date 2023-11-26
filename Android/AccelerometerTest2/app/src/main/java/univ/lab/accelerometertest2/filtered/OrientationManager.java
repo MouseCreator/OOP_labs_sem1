29a2fa19a043726b97c6a830e8e984adc32a8572
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-11-26 17:45:01 +0200

    added orientation manager class

---
+++ Android/AccelerometerTest2/app/src/main/java/univ/lab/accelerometertest2/filtered/OrientationManager.java
@@ -0,0 +1,128 @@
+package univ.lab.accelerometertest2.filtered;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import univ.lab.accelerometertest2.Vector3;
+
+public class OrientationManager {
+    private Vector3 currentVector = null;
+
+    public Vector3 getCurrentVector() {
+        return currentVector;
+    }
+
+    private final Context context;
+
+    public OrientationManager(Context context) {
+        this.context = context;
+    }
+
+    public void start() {
+        create();
+        resumeSensors();
+    }
+    public void stop() {
+        pauseSensors();
+        destroyAll();
+    }
+    private void destroyAll() {
+        mGyroscope = null;
+        mAccelerometer = null;
+        mMagnetic = null;
+        orientationCalculator = null;
+        mSensorManager = null;
+        magneticWrapper = null;
+        accelerometerWrapper = null;
+        gyroscopeWrapper = null;
+        magneticEventListener = null;
+        accelerometerEventListener = null;
+        gyroscopeEventListener = null;
+        currentVector = null;
+    }
+    private Sensor mGyroscope;
+    private Sensor mAccelerometer;
+    private Sensor mMagnetic;
+    private OrientationCalculator orientationCalculator;
+    private SensorManager mSensorManager;
+
+    private SensorWrapper magneticWrapper;
+    private SensorWrapper accelerometerWrapper;
+    private SensorWrapper gyroscopeWrapper;
+    private SensorEventListener magneticEventListener;
+    private SensorEventListener accelerometerEventListener;
+    private SensorEventListener gyroscopeEventListener;
+
+    private void create() {
+        mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        advancedGyroListener();
+    }
+
+    private void initAccelerometerEventListener() {
+        accelerometerEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                accelerometerWrapper.update(sensorEvent);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+    }
+
+    private void initGyroscopeEventListener() {
+        gyroscopeEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                gyroscopeWrapper.update(sensorEvent);
+                currentVector = orientationCalculator.updateAndGet();
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+    }
+
+    private void advancedGyroListener() {
+        orientationCalculator = new OrientationCalculator();
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mMagnetic = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        accelerometerWrapper = new SensorWrapper(null);
+        magneticWrapper = new SensorWrapper(null);
+        gyroscopeWrapper = new SensorWrapper(null);
+        initAccelerometerEventListener();
+        initMagneticEventListener();
+        initGyroscopeEventListener();
+        orientationCalculator.init(accelerometerWrapper, magneticWrapper, gyroscopeWrapper);
+    }
+    private void initMagneticEventListener() {
+        magneticEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                magneticWrapper.update(sensorEvent);
+            }
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+    }
+
+    public void resumeSensors() {
+        mSensorManager.registerListener(gyroscopeEventListener, mGyroscope, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(accelerometerEventListener, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(magneticEventListener, mMagnetic, SensorManager.SENSOR_DELAY_GAME);
+    }
+    private void pauseSensors() {
+        mSensorManager.unregisterListener(accelerometerEventListener);
+        mSensorManager.unregisterListener(gyroscopeEventListener);
+        mSensorManager.unregisterListener(magneticEventListener);
+    }
+}
