62c0f45f9873d23173e5ef4c5ef89f2615e93fb0
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-11-27 17:36:13 +0200

    added game server class

---
+++ OOPLab3/OOPLab3A/src/main/java/org/example/server/GameServer.java
@@ -0,0 +1,53 @@
+package org.example.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class GameServer {
+    private final ConcurrentLinkedQueue<String> messageQueue = new ConcurrentLinkedQueue<>();
+    public void start(int port) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            ClientHandler clientHandler = new ClientHandler(serverSocket.accept(), messageQueue);
+            clientHandler.process();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class ClientHandler {
+        private final Socket clientSocket;
+        private BufferedReader in;
+        private final ConcurrentLinkedQueue<String> messageQueue;
+        public ClientHandler(Socket socket, ConcurrentLinkedQueue<String> queue) {
+            this.clientSocket = socket;
+            this.messageQueue = queue;
+        }
+
+        public void process() {
+            try {
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    messageQueue.offer(inputLine);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    in.close();
+                    clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public ConcurrentLinkedQueue<String> getMessageQueue() {
+        return messageQueue;
+    }
+}
