1ab0d9ec4b9c1602cd418c0e03d42d3898fd1525
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-11-28 00:57:12 +0200

    added sizes class

---
+++ OOPLab3/OOPLab3A/src/main/java/org/example/collision/Sizes.java
@@ -0,0 +1,12 @@
+package org.example.collision;
+
+import org.example.model.Vector3D;
+
+public class Sizes {
+    public static Vector3D dummySize() {
+        return new Vector3D(160, 240, 40);
+    }
+    public static Vector3D shurikenSize() {
+        return new Vector3D(80, 5, 80);
+    }
+}
