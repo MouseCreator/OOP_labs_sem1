715b11ec04eb0cde5bc886acf78658beebbb4e03
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-12-05 02:19:56 +0200

    added bat picture

--- OOPLab3/OOPLab3A/src/main/java/org/example/game/movement/SinusMovement.java
+++ OOPLab3/OOPLab3A/src/main/java/org/example/game/movement/SinusMovement.java
@@ -7,17 +7,19 @@ public class SinusMovement implements Movement{
     private final double amplitude;
     private final double frequency;
     private final double step;
+    private final double phase;
 
-    public SinusMovement(double amplitude, double frequency, double step, double xSpeed) {
+    public SinusMovement(double amplitude, double frequency, double step, double phase, double xSpeed) {
         this.amplitude = amplitude;
         this.frequency = frequency;
         this.step = step;
         this.xSpeed = xSpeed;
+        this.phase = phase;
     }
 
     @Override
     public Vector3D nextPosition(Vector3D cp) {
-        double currentTime = cp.z() * frequency / amplitude;
+        double currentTime = cp.z() * frequency / amplitude + phase;
         double newZ = amplitude * Math.sin(2 * Math.PI * frequency * (currentTime + step));
         return new Vector3D(cp.x() + xSpeed, cp.y(), newZ);
     }
