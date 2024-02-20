603cbf1e58d4bcf1286df404033a151fe9226454
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-02-20 21:30:40 +0200

    Update repo structure

--- Android/AccelerometerTest2/app/src/main/java/univ/lab/accelerometertest2/filtered/OrientationCalculator.java
+++
@@ -1,8 +0,0 @@
-package univ.lab.accelerometertest2.filtered;
-
-public interface OrientationCalculator {
-    void init(SensorWrapper accWrapper, SensorWrapper magWrapper, SensorWrapper gyroWrapper);
-    Vector3 getOrientation();
-    Vector3 updateAndGet();
-    void resetVectors();
-}
