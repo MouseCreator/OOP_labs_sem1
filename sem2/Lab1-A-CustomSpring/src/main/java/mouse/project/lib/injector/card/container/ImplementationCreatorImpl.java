b35d2bc4d5c84753a5b2a6fb33e24d9da570718c
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-03-19 15:21:29 +0200

    add prototype annotation

--- sem2/Lab1-A-CustomSpring/src/main/java/mouse/project/lib/injector/card/container/ImplementationCreatorImpl.java
+++ sem2/Lab1-A-CustomSpring/src/main/java/mouse/project/lib/injector/card/container/ImplementationCreatorImpl.java
@@ -2,6 +2,7 @@ package mouse.project.lib.injector.card.container;
 
 import mouse.project.lib.annotation.Name;
 import mouse.project.lib.annotation.Primary;
+import mouse.project.lib.annotation.Prototype;
 
 public class ImplementationCreatorImpl implements ImplementationCreator {
     @Override
@@ -11,6 +12,9 @@ public class ImplementationCreatorImpl implements ImplementationCreator {
         Implementation<T> implementation = new Implementation<>(tClass, name);
         if (tClass.isAnnotationPresent(Primary.class)) {
             implementation.setPrimary(true);
+        }
+        if (tClass.isAnnotationPresent(Prototype.class)) {
+            implementation.setPrototype(true);
         }
         return implementation;
     }
