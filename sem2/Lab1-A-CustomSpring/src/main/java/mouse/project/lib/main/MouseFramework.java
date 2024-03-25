b4ee15df98634af9ffd9c3d860c9dcb4494dc5a6
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-03-25 13:42:19 +0200

    remove mouse framework class

--- sem2/Lab1-A-CustomSpring/src/main/java/mouse/project/lib/main/MouseFramework.java
+++
@@ -1,9 +0,0 @@
-package mouse.project.lib.main;
-
-import java.util.Arrays;
-
-public class MouseFramework {
-    public void start(String[] args) {
-        System.out.println(Arrays.toString(args));
-    }
-}
