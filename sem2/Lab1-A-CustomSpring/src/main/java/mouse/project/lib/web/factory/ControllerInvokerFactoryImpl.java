99fa85fa1bb8894886bd3ecf0ca6914506fc782f
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-03-26 18:11:25 +0200

    mock controller invoker

--- sem2/Lab1-A-CustomSpring/src/main/java/mouse/project/lib/web/factory/ControllerInvokerFactoryImpl.java
+++ sem2/Lab1-A-CustomSpring/src/main/java/mouse/project/lib/web/factory/ControllerInvokerFactoryImpl.java
@@ -1,24 +1,15 @@
 package mouse.project.lib.web.factory;
 
-import mouse.project.lib.ioc.annotation.Auto;
 import mouse.project.lib.ioc.annotation.Service;
-import mouse.project.lib.web.factory.translations.ParamTranslationFactory;
 import mouse.project.lib.web.invoker.*;
 
 import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
 
 @Service
 public class ControllerInvokerFactoryImpl implements ControllerInvokerFactory {
-    private final ParamTranslationFactory paramTranslationFactory;
-    @Auto
-    public ControllerInvokerFactoryImpl(ParamTranslationFactory paramTranslationFactory) {
-        this.paramTranslationFactory = paramTranslationFactory;
-    }
 
     @Override
     public ControllerInvoker create(Object controller, Method method) {
-        Parameter[] parameters = method.getParameters();
         return null;
     }
 }
